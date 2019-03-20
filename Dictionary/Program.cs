6aabec7fd2f7c8b762d3bfffe49b89cf58583bfd
Peter Starup <pete59f1@edu.eal.dk>
2019-03-20 09:13:46 +0100

    Add project files.

---
+++ Dictionary/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Dictionary
+{
+    public enum MemberType
+    {
+        active, passive
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MemberUI ui = new MemberUI(new MemberController());
+            ui.Run();
+        }
+    }
+    //Ex64 øvelse 1
+    //Der er anvendt en tre lags arkitektur
+    //
+}
